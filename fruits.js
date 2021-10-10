7084c8b9143ecd6c14d0bfebbf33a302ea80c96b
YatharthDive <71880750+YatharthDive@users.noreply.github.com>
2021-10-10 20:21:17 +0530

    Add files via upload

---
+++ fruits.js
@@ -0,0 +1,32 @@
+img="";
+status1=""
+function preload(){
+    img=loadImage("fruits.jpg")
+}
+function setup(){
+C1=createCanvas(640,420)
+C1.center()
+objectDetector=ml5.objectDetector("cocossd",modelLoaded)
+}
+function modelLoaded(){
+    console.log("model Loaded")
+    status1=true
+    objectDetector.detect(img,gotResults)
+}
+function gotResults(error,results) {
+    if (error) {
+        console.log(error)
+    }
+    console.log(results)
+}
+function draw(){
+    image(img,0,0,640,420)
+    fill("red")
+    text("bed",130,100)
+    noFill()
+    stroke("red")
+    rect(125,90,400,350)
+}
+function back(){
+    window.location.href= "index.html";
+}
---
+++ main2.js
@@ -0,0 +1,9 @@
+function callBD(){
+    window.location.href= "bedroom.html";
+}
+function callFRUITS(){
+    window.location.href= "fruits.html";
+}
+function callDESK(){
+    window.location.href= "desk.html";
+}
