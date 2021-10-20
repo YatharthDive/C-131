5f8311177e9cf67118be4fc16f384fcc15a86a6a
YatharthDive <71880750+YatharthDive@users.noreply.github.com>
2021-10-20 18:52:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,31 +1,47 @@
-img="";
-status1=""
-function preload(){
-    img=loadImage("BEDROOM.jpg")
+img = "";
+status1 = ""
+objects = []
+
+function preload() {
+    img = loadImage("BEDROOM.jpg")
 }
-function setup(){
-C1=createCanvas(640,420)
-C1.center()
-objectDetector=ml5.objectDetector("cocossd",modelLoaded)
+
+function setup() {
+    C1 = createCanvas(640, 420)
+    C1.center()
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded)
+    document.getElementById("status").innerHTML="status : Detecting Object"
 }
-function modelLoaded(){
+
+function modelLoaded() {
     console.log("model Loaded")
-    status1=true
-    objectDetector.detect(img,gotResults)
+    status1 = true
+    objectDetector.detect(img, gotResults)
 }
-function gotResults(error,results) {
+
+function gotResults(error, results) {
     if (error) {
         console.log(error)
+    } else {
+        console.log(results)
+        objects=results
     }
-    console.log(results)
+
 }
-function draw(){
-    image(img,0,0,640,420)
-    fill("red")
-    text("bed",130,100)
-    noFill()
-    stroke("red")
-    rect(125,90,400,350)
+
+function draw() {
+    image(img, 0, 0, 640, 420)
+    if(status1!=""){
+        for(var I=0;I<objects.length;I++){
+            document.getElementById("status").innerHTML="status : Detected Object"
+            fill("red")
+            percent=floor(objects[I].confidence*100)
+            text(objects[I].label+" "+percent+"%", objects[I].x-50, objects[I].y-50)
+            noFill()
+            stroke("red")
+            rect(objects[I].x,objects[I].y,objects[I].width,objects[I].height)
+        }
+    }
 }
 function back(){
     window.location.href= "index.html";
